423ac4205e68e7358a1ba7fade3500d8911ee622
CAVOINHO <kentnguyen101@gmail.com>
2021-01-30 16:54:39 +0700

    Day7 - Back End User Authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,13 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productControllers.js'
 
 const router = express.Router()
 
-//* @desc       Fetch single product
-//* @route      GET /api/products/:id
-//* @access     Public
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    // res.status(401)
-    // throw new Error('Some error')
-    res.json(products)
-  })
-)
+router.route('/').get(getProducts)
 
-//* @desc       Fetch all products
-//* @route      GET /api/products
-//* @access     Public
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-)
+router.route('/:id').get(getProductById)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import connectDB from './config/db.js'
 import { errorHandle, notFound } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -11,7 +12,10 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('API is running...')
